0424c28f1f520b19329a4cc2e6adef4c650a1479
Simon Goldin <simon.goldin@dashlane.com>
2020-05-24 17:45:45 -0400

    replace with gatsby-starter-typescript-plus

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,66 +1,50 @@
 import * as React from 'react';
-import Link from 'gatsby-link';
 import { Helmet } from 'react-helmet';
+import { StaticQuery, graphql } from 'gatsby';
 
-import './module.index.scss';
+import 'modern-normalize';
+import '../styles/normalize';
 
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+import Header from '../components/Header';
+import LayoutRoot from '../components/LayoutRoot';
+import LayoutMain from '../components/LayoutMain';
 
-interface DefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
-  location: {
-    pathname: string
-  }
-  children: any
+interface StaticQueryProps {
+  site: {
+    siteMetadata: {
+      title: string;
+      description: string;
+      keywords: string;
+    };
+  };
 }
 
-const DefaultLayout = (props: DefaultLayoutProps) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' }
-      ]}
-    />
-    <Header/>
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0
-      }}
-    >
-      test
-    </div>
-  </div>
+const IndexLayout: React.FC = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query IndexLayoutQuery {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `}
+    render={(data: StaticQueryProps) => (
+      <LayoutRoot>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: data.site.siteMetadata.description },
+            { name: 'keywords', content: data.site.siteMetadata.keywords }
+          ]}
+        />
+        <Header title={data.site.siteMetadata.title} />
+        <LayoutMain>{children}</LayoutMain>
+      </LayoutRoot>
+    )}
+  />
 );
 
-export default DefaultLayout
+export default IndexLayout;
